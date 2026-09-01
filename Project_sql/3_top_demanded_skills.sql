07763c6df1cfb3d40f53aa4881ae7fe54fc0f3e0
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-01 13:52:35 +0530

    style: satisfy core SQLFluff layout rules

--- Project_sql/3_top_demanded_skills.sql
+++ Project_sql/3_top_demanded_skills.sql
@@ -6,7 +6,8 @@ INNER JOIN skills_job_dim AS sj
     ON j.job_id = sj.job_id
 INNER JOIN skills_dim AS s
     ON sj.skill_id = s.skill_id
-WHERE j.job_title_short = 'Data Analyst'
+WHERE
+    j.job_title_short = 'Data Analyst'
     AND j.job_work_from_home IS TRUE
 GROUP BY s.skills
 ORDER BY skill_count DESC, s.skills
