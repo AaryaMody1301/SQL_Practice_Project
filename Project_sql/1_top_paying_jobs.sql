b71a542f0c7caec9a7564f7d0376d50f6e3ef4b8
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-01 13:52:22 +0530

    style: satisfy core SQLFluff layout rules

--- Project_sql/1_top_paying_jobs.sql
+++ Project_sql/1_top_paying_jobs.sql
@@ -9,7 +9,8 @@ SELECT
 FROM job_postings_fact AS j
 LEFT JOIN company_dim AS c
     ON j.company_id = c.company_id
-WHERE j.job_title_short = 'Data Analyst'
+WHERE
+    j.job_title_short = 'Data Analyst'
     AND j.job_work_from_home IS TRUE
     AND j.salary_year_avg IS NOT NULL
 ORDER BY j.salary_year_avg DESC, j.job_id
