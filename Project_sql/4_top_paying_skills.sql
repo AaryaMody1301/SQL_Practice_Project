79813bc51ef663c02b759cb3d00fc59313fb0bf6
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-01 13:48:31 +0530

    fix: align remote skill salary population

--- Project_sql/4_top_paying_skills.sql
+++ Project_sql/4_top_paying_skills.sql
@@ -1,15 +1,14 @@
-SELECT 
-    skills,
-    ROUND(AVG(salary_year_avg), 0) AS avg_salary
-FROM 
-    job_postings_fact
-INNER JOIN 
-    skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN 
-    skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE 
-    job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL
-GROUP BY 
-    skills
-ORDER BY
-    avg_salary DESC
+SELECT
+    s.skills,
+    ROUND(AVG(j.salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact AS j
+INNER JOIN skills_job_dim AS sj
+    ON j.job_id = sj.job_id
+INNER JOIN skills_dim AS s
+    ON sj.skill_id = s.skill_id
+WHERE j.job_title_short = 'Data Analyst'
+    AND j.job_work_from_home IS TRUE
+    AND j.salary_year_avg IS NOT NULL
+GROUP BY s.skills
+ORDER BY avg_salary DESC, s.skills
+LIMIT 25;
