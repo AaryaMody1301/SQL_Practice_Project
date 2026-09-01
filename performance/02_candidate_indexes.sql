fc38f79d26b3c1f9aa0b9ef79f25df3494b4be22
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-01 17:44:55 +0530

    style: match partial-index predicate layout

--- performance/02_candidate_indexes.sql
+++ performance/02_candidate_indexes.sql
@@ -8,9 +8,9 @@ INCLUDE (
     job_posted_date
 )
 WHERE
-    job_title_short = 'Data Analyst'
-    AND job_work_from_home IS TRUE
-    AND salary_year_avg IS NOT NULL;
+job_title_short = 'Data Analyst'
+AND job_work_from_home IS TRUE
+AND salary_year_avg IS NOT NULL;
 
 CREATE INDEX idx_job_postings_da_work_mode_job
 ON public.job_postings_fact (job_work_from_home, job_id)
@@ -20,6 +20,6 @@ INCLUDE (
     salary_year_avg
 )
 WHERE
-    job_title_short = 'Data Analyst';
+job_title_short = 'Data Analyst';
 
 ANALYZE public.job_postings_fact;
